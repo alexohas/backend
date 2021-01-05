f005fe191db3498c458f75f409634e1a294ec6ee
Ahman Emmanuel <ahmanemmanuel@gmail.com>
2021-01-05 02:20:28 +0100

    init

--- index.js
+++ index.js
@@ -3,85 +3,61 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
-
-const Product = require("./models/product");
+var cors = require('cors')
+const Lessons = require("./models/lessons");
+const Orders = require("./models/orders");
 
 //Connecting to mongodb.
 mongoose
-  .connect("mongodb://localhost:27017/farmStand", {
+  .connect("mongodb+srv://tech:Wittercell@development.8h65w.mongodb.net/after-school?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("Mongo Connection Open");
+      console.log("Mongo Connection Open");
+      app.use(cors("*"))
+
+    // app.use(express.urlencoded({ extended: true }));
+    app.use(methodOverride("_method"));
+    app.use(express.json()) 
+    app.use (express.urlencoded({extended: false}))
+    //SHOW PRODUCTS
+    app.get("/lessons", async (req, res) => {
+        const lessons = await Lessons.find({});
+        res.json({ lessons });
+    });
+      
+      //SAVE ORDER
+      app.post("/orders", async (req, res) => {
+          let { contents, user, total } = req.body
+          const order = await Orders.create({ contents, user, total });
+          for (let item of contents) {
+              await Lessons.update({_id: item.lesson}, {$inc: {space: -1*item.quantity}})
+          }
+        res.json({ order, message: "Order placed successfully" });
+      });
+      
+      app.post("/fetch-orders", async (req, res) => {
+        let { user } = req.body
+          const orders = await Orders.find({
+              $or: [{
+            'user.name': user.name
+        }, {'user.phone': user.phone}] }).populate("contents.lesson");
+      res.json({ orders, message: "Orders retrieved successfully" });
+  });
+
+
+    //SHOW SPECIFIC PRODUCT
+    app.get("/lessons/:id", async (req, res) => {
+        const { id } = req.params;
+        const lesson = await Lessons.findById(id);
+        res.json({ lesson });
+    });
+
+    app.listen(1002, () => {
+        console.log("App is listening at port 1002");
+    });
   })
   .catch((err) => {
     console.log("Mongo Connection Error");
   });
-
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-
-const categories = ["fruit", "vegetable", "dairy", "fungi", "baked goods"];
-
-//SHOW PRODUCTS
-app.get("/products", async (req, res) => {
-  const { category } = req.query;
-  if (category) {
-    const products = await Product.find({ category });
-    res.render("products/index", { products, category });
-  } else {
-    const products = await Product.find({});
-    res.render("products/index", { products, category: "All" });
-  }
-});
-
-//ADD A PRODUCT
-app.post("/products", async (req, res) => {
-  const product = new Product(({ name, price, category } = req.body));
-  await product.save();
-  res.redirect(`/products/${product._id}`);
-});
-
-//SHOW NEW PRODUCT FORM
-app.get("/products/new", (req, res) => {
-  res.render("products/new", { categories });
-});
-
-//SHOW SPECIFIC PRODUCT
-app.get("/products/:id", async (req, res) => {
-  const { id } = req.params;
-  const product = await Product.findById(id);
-  res.render("products/show", { product });
-});
-
-//SHOW EDIT PRODUCT FORM
-app.get("/products/:id/edit", async (req, res) => {
-  const { id } = req.params;
-  const product = await Product.findById(id);
-  res.render("products/edit", { product, categories });
-});
-
-//UPDATE PRODUCT
-app.put("/products/:id", async (req, res) => {
-  const { id } = req.params;
-  const product = await Product.findByIdAndUpdate(id, req.body, {
-    runValidators: true,
-    new: true,
-  });
-  res.redirect(`/products/${product._id}`);
-});
-
-//DELETE PRODUCT
-app.delete("/products/:id", async (req, res) => {
-  const { id } = req.params;
-  await Product.findByIdAndDelete(id);
-  res.redirect(`/products`);
-});
-
-app.listen(8080, () => {
-  console.log("App is listening at port 8080");
-});
