0c8d190d5b4a39a2580628a12879675110dd0e2e
Ahman Emmanuel <ahmanemmanuel@gmail.com>
2021-01-05 02:20:08 +0100

    data seed

--- seeds.js
+++ seeds.js
@@ -1,45 +1,56 @@
 const mongoose = require("mongoose");
-
-const Product = require("./models/product");
+var randomWords = require("random-words");
+const Lessons = require("./models/lessons");
+const Orders = require("./models/orders");
 
 //Connecting to mongodb.
 mongoose
-  .connect("mongodb://localhost:27017/farmStand", {
+  .connect("mongodb+srv://tech:Wittercell@development.8h65w.mongodb.net/after-school?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("Mongo Connection Open");
+      console.log("Mongo Connection Open");
+      const locations = ["Abbeville", "Abbotsford", "Abbott", "Abbottstown", "Abercrombie", "Aberdeen", "Aberdeen Proving Ground", "Abernathy", "Abilene", "Abingdon", "Abington", "Abington Township", "Abiquiu", "Abita Springs", "Abrams", "Absarokee", "Absecon", "Acampo", "Access", "Accident", "Accokeek", "Accomac", "Accord", "Accoville", "Achille", "Achilles", "Ackerly", "Ackerman", "Ackley", "Acme", "Acton", "Acushnet", "Acworth", "Ada", "Adah", "Adair", "Adairsville", "Adairville", "Adak", "Adams", "Adams Center", "Adams Run", "Adamstown", "Adamsville", "Addieville", "Addis", "Addison", "Addyston", "Adel", "Adelanto", "Adell", "Adena", "Adger", "Adin", "Adkins", "Admire", "Adolphus", "Adona", "Adrian", "Advance", "Afton", "Agar", "Agate", "Agawam", "Agency", "Agoura Hills", "Agra"]
+Lessons.deleteMany({}).then(val => {
+    console.log("cleared")
+}).catch(error => {
+    console.log(error.message)
+})
+Orders.deleteMany({}).then(val => {
+    console.log("cleared")
+}).catch(error => {
+    console.log(error.message)
+})
+      for (let index = 1; index <= 30; index++) {
+          let ph = index + 10
+          if (ph === 30) {
+              ph = 02
+          }
+          if (ph === 34) {
+            ph = 03
+        }
+          if (ph === 17) {
+              ph = 01
+          }
+    let lesson = {
+        lesson: randomWords({ exactly: 2, join: " " }),
+        location: locations[index],
+        price: index>10?index * 2: index*3,
+        space: 5,
+        image: "https://picsum.photos/id/10"+ph+"/200/300"
+    }
+    Lessons.insertMany(lesson).then((data) => {
+        // console.log(data)
+    }).catch(error => {
+        console.log(error.message)
+    })
+}
   })
   .catch((err) => {
     console.log("Mongo Connection Error");
   });
 
-// const p = new Product({
-//   name: "Ruby Grapefruits",
-//   price: 1.99,
-//   category: "fruit",
-// });
 
-// p.save()
-//   .then((p) => {
-//     console.log(p);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
 
-const seedProducts = [
-  { name: "Fairy Eggplant", price: 1.0, category: "vegetable" },
-  { name: "Organic Goddess Melon", price: 4.99, category: "fruit" },
-  { name: "Organic Mini Seedless Watermelon", price: 3.99, category: "fruit" },
-  { name: "Organic Celery", price: 1.5, category: "vegetable" },
-  { name: "Chocolate Whole Milk", price: 2.69, category: "dairy" },
-];
-Product.insertMany(seedProducts)
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+
